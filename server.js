5ac90a5c4c55b10e0e16f912958351ff7e7d7238
T4T4el7 <ochoaarmando13@gmail.com>
2019-05-20 10:18:26 -0700

    fix

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Use apiRoutes
+// Use apiRoutes GET
 //app.use("/api", apiRoutes);
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // app.listen(PORT, function() {
